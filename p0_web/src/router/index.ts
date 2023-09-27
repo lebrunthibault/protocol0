58abaae163c9000e258c050bd6816dbca4d825d5
Thibault <thibaultlebrun@live.fr>
2023-09-27 22:12:44 +0200

    'Backup'

--- p0_web/src/router/index.ts
+++ p0_web/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AbletonSetView from '../views/AbletonSet.vue'
-import ActionView from '../views/ScriptActions.vue'
-import HomeView from '../views/Home.vue'
+import AbletonSetView from '../views/AbletonSetView.vue'
+import ActionView from '../views/ScriptActionsView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
