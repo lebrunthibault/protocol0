b4abaf1a01b3ab00c2836206361f505b0e42ed05
Thibault <thibaultlebrun@live.fr>
2024-01-18 13:52:33 +0100

    'Backup'

--- p0_script/protocol0/domain/lom/set/MixingService.py
+++ p0_script/protocol0/domain/lom/set/MixingService.py
@@ -1,7 +1,10 @@
 from protocol0.domain.lom.track.CurrentMonitoringStateEnum import CurrentMonitoringStateEnum
 from protocol0.domain.lom.track.group_track.NormalGroupTrack import NormalGroupTrack
+from protocol0.domain.lom.track.routing.OutputRoutingTypeEnum import OutputRoutingTypeEnum
+from protocol0.domain.lom.track.simple_track.SimpleTrack import SimpleTrack
 from protocol0.domain.lom.track.simple_track.audio.SimpleAudioTrack import SimpleAudioTrack
 from protocol0.shared.Song import Song
+from protocol0.shared.logging.Logger import Logger
 
 
 class MixingService(object):
@@ -17,3 +20,13 @@ class MixingService(object):
                 continue
 
             track.base_track.scroll_volume(go_next)
+
+    def log_state(self) -> None:
+        def is_zero_volume(t: SimpleTrack) -> bool:
+            return not t.is_foldable and t.volume < -45
+
+        def routed_to_master(t: SimpleTrack) -> bool:
+            return t.output_routing.type == OutputRoutingTypeEnum.MASTER
+
+        Logger.info(f"zero_volume_tracks: {list(filter(is_zero_volume, Song.simple_tracks()))}")
+        Logger.info(f"routed to master: {list(filter(routed_to_master, Song.simple_tracks()))}")
