6ac3a3d9ac931eb4b96af3d1860422c8171c258d
Thibault <thibaultlebrun@live.fr>
2024-05-07 03:48:53 +0200

    'Backup'

--- p0_backend/p0_backend/api/http_server/routes/search_routes.py
+++ p0_backend/p0_backend/api/http_server/routes/search_routes.py
@@ -8,6 +8,7 @@ from p0_backend.api.client.p0_script_api_client import p0_script_client
 from p0_backend.lib.ableton.ableton import focus_ableton
 from p0_backend.lib.ableton.ableton_set.ableton_set_manager import AbletonSetManager
 from p0_backend.lib.errors.Protocol0Error import Protocol0Error
+from p0_backend.lib.keys import send_keys
 from p0_backend.lib.notification import notify
 from p0_backend.lib.window.window import focus_window_by_handle
 from protocol0.application.command.SelectTrackCommand import SelectTrackCommand
@@ -20,6 +21,9 @@ async def _search_track() -> None:
     thread = Thread(target=search_track)
     thread.start()
     thread.join()
+
+
+track_list_history: set[str] = set()
 
 
 def search_track() -> None:
@@ -57,7 +61,10 @@ def search_track() -> None:
 
     def track_list_from_substring(value: str) -> List[str]:
         if value == "":
-            return track_list
+            if track_list_history:
+                return list(track_list_history)
+            else:
+                return track_list
 
         data = []
         for item in track_list:
@@ -73,22 +80,28 @@ def search_track() -> None:
 
     def update_autocomplete(data) -> None:
         # clear previous data
-        lb.delete(0, "end")
+        list_box.delete(0, "end")
 
         # put new data
         for item in data:
-            lb.insert("end", item)
+            list_box.insert("end", item)
 
-    lb = tk.Listbox(root)
-    lb.pack()
+    list_box = tk.Listbox(root)
+    list_box.pack()
     update_autocomplete(track_list)
 
-    def onselect(evt) -> None:
+    def on_select(evt) -> None:
         index = int(evt.widget.curselection()[0])
         submit(evt.widget.get(index))
 
-    lb.bind("<Return>", onselect)
-    lb.bind("<Double-1>", onselect)
+    list_box.bind("<Return>", on_select)
+    list_box.bind("<Double-1>", on_select)
+
+    def on_down(_) -> None:
+        if not list_box.curselection():
+            send_keys("{TAB}")
+
+    list_box.bind_all("<Down>", on_down)
 
     def get_input() -> None:
         submit(entry.get())
@@ -100,6 +113,7 @@ def search_track() -> None:
             if track_sub_list:
                 track_name = track_sub_list[0]
 
+        track_list_history.add(track_name)
         p0_script_client().dispatch(SelectTrackCommand(track_name))
 
         autoclose_timer.cancel()
